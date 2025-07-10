e3b223549f97a58b5d5897533b2469f4b9fce00f
Evan Smith <evanjs71504@gmail.com>
2025-07-10 10:45:58 -0400

    base_code

---
+++ L_M_S/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+// -------------------- Book Class --------------------
+class Book {
+    // TODO: Declare attributes and methods
+};
+
+// -------------------- User Class --------------------
+class User {
+    // TODO: Declare attributes and methods
+};
+
+// -------------------- BorrowTransaction Class --------------------
+class BorrowTransaction {
+    // TODO: Declare attributes and methods
+};
+
+// -------------------- Library Class --------------------
+class Library {
+    // TODO: Declare attributes and methods
+};
